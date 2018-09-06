48d3d8438314955244f217ad323162ba5c848382
ICHIRO KAWATA <ichirokawata@ichironomacbook.local>
2018-09-07 01:34:28 +0900

    Instagram 完成

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by ICHIRO KAWATA on 2018/09/05.
+//  Copyright © 2018年 ICHIRO KAWATA. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
